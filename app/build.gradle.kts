568d67a2484206174ee680810243b1fc8a8c9564
Laury PRIN <prinlauryy@gmail.com>
2025-10-09 09:28:03 +0200

    AJout dans les gradle des dependance nécessaire pour Hilt, Moshi et Retrofit. Initialisation de Hilt faite: création fichier app et des entrypoint pour les activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
+  kotlin("kapt")
+  id("com.google.dagger.hilt.android")
 }
 
 android {
   namespace = "com.aura"
-  compileSdk = 33
+  compileSdk = 34
 
   defaultConfig {
     applicationId = "com.aura"
     minSdk = 24
-    targetSdk = 33
+    targetSdk = 34
     versionCode = 1
     versionName = "1.0"
 
@@ -36,13 +38,34 @@ android {
 }
 
 dependencies {
+  // Hilt
+  implementation("com.google.dagger:hilt-android:2.50")
+  kapt("com.google.dagger:hilt-android-compiler:2.50")
 
+  // AndroidX et Activity components
   implementation("androidx.core:core-ktx:1.9.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.google.android.material:material:1.8.0")
   implementation("androidx.annotation:annotation:1.6.0")
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+  // Moshi
+  implementation("com.squareup.moshi:moshi-kotlin:1.15.0")
+
+  // Retrofit
+  implementation("com.squareup.retrofit2:retrofit:2.9.0")
+  implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+  implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+  // Test
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+  testImplementation("io.mockk:mockk:1.13.9")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0-RC2")
+
+}
+
+kapt {
+  correctErrorTypes = true
 }
