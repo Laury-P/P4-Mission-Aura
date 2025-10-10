1dc59024747841e6b069d5961c37ead01d2b9762
Laury PRIN <prinlauryy@gmail.com>
2025-10-10 12:08:58 +0200

    SuccesState correctement implémenter

--- app/src/main/java/com/aura/ui/login/LoginActivity.kt
+++ app/src/main/java/com/aura/ui/login/LoginActivity.kt
@@ -4,20 +4,22 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.aura.databinding.ActivityLoginBinding
 import com.aura.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
+import com.aura.R
 import kotlinx.coroutines.launch
+import com.aura.data.repository.Result
 
 /**
  * The login activity for the app.
  */
 @AndroidEntryPoint
-class LoginActivity : AppCompatActivity()
-{
+class LoginActivity : AppCompatActivity() {
 
   /**
    * The binding for the login layout.
@@ -25,8 +27,7 @@ class LoginActivity : AppCompatActivity()
   private lateinit var binding: ActivityLoginBinding
   private val viewModel: LoginVewModel by viewModels()
 
-  override fun onCreate(savedInstanceState: Bundle?)
-  {
+  override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 
     binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -46,20 +47,31 @@ class LoginActivity : AppCompatActivity()
       viewModel.onPasswordChanged(it.toString())
     }
 
-    lifecycleScope.launch{
-      viewModel.isLoginEnabled.collect{ login.isEnabled = it }
+    lifecycleScope.launch {
+      viewModel.isLoginEnabled.collect { login.isEnabled = it }
     }
 
     login.setOnClickListener {
-      loading.visibility = View.VISIBLE
+      lifecycleScope.launch {
+        viewModel.login(identifier.text.toString(), password.text.toString())
+        viewModel.loginState.collect {
+          loading.visibility = if (it.loading) View.VISIBLE else View.GONE
+          if (it.loginResult == true) {
+            Toast.makeText(this@LoginActivity, getString(R.string.login_success), Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+            startActivity(intent)
+          }
+          if (it.loginResult == false) {
+            Toast.makeText(this@LoginActivity, getString(R.string.login_refused), Toast.LENGTH_SHORT).show()
+          }
+          if (it.errorMessage != null) {
+            Toast.makeText(this@LoginActivity, it.errorMessage, Toast.LENGTH_SHORT).show()
+          }
+        }
+      }
 
-      val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-      startActivity(intent)
-
-      finish()
     }
 
+
   }
-
-
 }
--- app/src/main/java/com/aura/ui/login/LoginVewModel.kt
+++ app/src/main/java/com/aura/ui/login/LoginVewModel.kt
@@ -1,20 +1,33 @@
 package com.aura.ui.login
 
-import androidx.lifecycle.LiveData
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.aura.R
+import com.aura.data.model.LoginResponse
+import com.aura.data.repository.AuraRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+import com.aura.data.repository.Result
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.update
 
 @HiltViewModel
-class LoginVewModel @Inject constructor(): ViewModel(){
+class LoginVewModel @Inject constructor(private val repository: AuraRepository): ViewModel(){
+
+    private val _loginState = MutableStateFlow(LoginState())
+    val loginState: StateFlow<LoginState> = _loginState.asStateFlow()
 
     private val _isLoginEnabled = MutableStateFlow(false)
     val isLoginEnabled: StateFlow<Boolean> = _isLoginEnabled.asStateFlow()
+
     private val _identifier = MutableStateFlow("")
     val identifier: StateFlow<String> = _identifier.asStateFlow()
+
     private val _password = MutableStateFlow("")
     val password: StateFlow<String> = _password.asStateFlow()
 
@@ -28,7 +41,46 @@ class LoginVewModel @Inject constructor(): ViewModel(){
         _password.value = password
         validateInputs()
     }
+
     private fun validateInputs(){
         _isLoginEnabled.value = _identifier.value.isNotEmpty() && _password.value.isNotEmpty()
     }
+
+    suspend fun login(identifier: String, password: String){
+        repository.login(identifier, password)
+            .onEach { result ->
+                when(result){
+                    is Result.Success -> _loginState.update{
+                        val granted = result.data.granted
+                        it.copy(
+                            loginResult = granted,
+                            loading = false,
+                            errorMessage = null,
+                        )
+                    }
+                    is Result.Error -> _loginState.update{
+                        Log.e("LoginViewModel", "Erreur login: ${result.exception.message}")
+                        it.copy(
+                            errorMessage = R.string.login_error.toString(),
+                            loading = false,
+                            loginResult = null,
+                        )
+                    }
+                    Result.Loading -> _loginState.update{
+                        it.copy(
+                            loading = true,
+                            errorMessage = null,
+                        )
+                    }
+                }
+            }
+            .launchIn(viewModelScope)
+    }
+
 }
+
+data class LoginState(
+    val loginResult: Boolean? = null,
+    val loading: Boolean = false,
+    val errorMessage: String? = null
+)
